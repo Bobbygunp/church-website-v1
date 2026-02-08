d47b7a7ea335a9c73957991e2c2b063168825bc0
Bobbygunp <prashanth.r.gundala@gmail.com>
2026-02-07 19:22:50 -0800

    feat: changed the Image in the this-month page

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,12 @@ const nextConfig: NextConfig = {
       {
          protocol: 'https',
          hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tcf-website-images.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
